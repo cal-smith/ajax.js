9e455d338f6af843a7af9fb86e36f193f3ebdae1
Callum Smith <callum1234moo@gmail.com>
2014-05-12 13:40:28 -0400

    added basic event binding

--- element1.js
+++ element1.js
@@ -1,26 +1,98 @@
 /*
 element1.js
+
+new ajax({
+	verb:(default: GET) GET|POST|PUT|DELETE,
+	url: "your.url.com",
+	url_var:["must","be","array"],
+	headers:{"key":"value"},
+	json: true|false,
+	progress: function(event){
+		//event handle
+	},
+	error: function(event){
+		//event handle
+	}
+}).done(function(data){
+	console.log(data);
+});
+
+.done = q.done. deffered request via promises.
+
+ajax.send({
+	verb:(default: GET) GET|POST|PUT|DELETE,
+	url: "your.url.com",
+	url_var:["must","be","array"],
+	headers:{"key":"value"},
+	json: true|false,
+	progress: function(event){
+		//event handle
+	},
+	error: function(event){
+		//event handle
+	}
+}, function(data){
+	console.log(data);
+});
+
+ajax.get|getJSON|post(url, {opt:data}, function(data){
+	
+});
 */
 'use strict';
-ajax = {//class for ajax interaction. as a bonus you can do basic requests via: ajax.send('VERB', url, function(var){ //etc });
-	//needs error handling, jsonp handling, non json request handling, progress handling, proper post handling
-	send: function(args, callback){// args takes object of: {verb, url, headers, json}
-		if (typeof args.json === "undefined"){
-			args.json = true;
+
+
+var ajax = {//usage: oldajax.send();
+	//needs error handling, jsonp handling, progress handling, proper post handling
+	send: function(args, callback){// args takes object of: {verb, url, headers, json, url_var} note: url_var must be an array
+		if (typeof args === "undefined" || typeof callback === "undefined"){
+			throw "Missing arguments";
+		} else{
+			if (typeof args.json === "undefined"){
+				args.json = true;
+			}
+			if (typeof args.verb === "undefined"){//!args.verb.match(/^(get|post|put|delete)$/i) ||
+				args.verb = 'get';//defaults to a GET request
+			}
 		}
+		if (typeof args.url_var !== "undefined"){//sets url vars
+			var parts = args.url_var;
+			args.url_var = "?"
+			for (var i = 0; i < parts.length; i++) {
+				if (i === 0) {
+					args.url_var += encodeURI(parts[i]);
+				} else {
+					args.url_var += "&" + encodeURI(parts[i]);
+				}
+			}
+			args.url += args.url_var;
+		}
+		//add http:// to url if ommited
+
+		//add check for support(lol) and ie activex
 		var req = new XMLHttpRequest();
+		/*
+		progress handlers
+		*/
+		if(typeof args.progress !== "undefined"){
+			req.addEventListener("progress", args.progress, false);
+		}
+		if(typeof args.progress !== "undefined"){
+			req.addEventListener("error", args.error, false);
+		}
+		
 		req.open(args.verb, args.url);
-		//req.setRequestHeader('Content-Type', 'text/xml');
 		if(typeof args.headers !== "undefined"){
 			headers.forEach(function(value, key){
 				req.setRequestHeader(key, value);
 			});
 		}
+
 		//set ready state
 		req.onload = res;
 		req.send();
 		function res(){
-			if (args.json){
+			if (args.json === true){
 				callback(JSON.parse(this.response), this.status);
 			} else {
 				callback(this.response, this.status);
