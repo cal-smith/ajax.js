bff43b46b6d1dcc57d62451acf42c63781fa3b94
Callum Smith <callum1234moo@gmail.com>
2015-04-05 03:17:06 -0400

    Moved some bits around to help make .send() smaller
    at this point it's probably close to as small as it can get
    might be an decent idea to drop the .verb method

--- ajax.js
+++ ajax.js
@@ -1,8 +1,10 @@
 (function(window){
 	"use strict";
 	var Ajax = function(url, verb){
+		this.req = new XMLHttpRequest();
 		this.req_verb = verb;
 		this.url = url;
+		this.parse_json = false;
 		return this;
 	};
 
@@ -12,50 +14,22 @@
 		//default to current page if no url is specified
 		typeof this.url === "undefined" && (this.url = window.location.href);
 
-		typeof this.parse_json === "undefined" && (this.parse_json = false);
-
 		if (typeof this.req_verb === "undefined" || !this.req_verb.match(/^(get|post|put|delete)$/i)){
 			this.req_verb = "get";
 		}
 
-		if (typeof this.url_var !== "undefined"){
-			var parts = this.url_var;
-			this.url_var = "?";
-			var keys = Object.keys(parts);
-			for (var i = 0; i < keys.length; i++) {
-				var k = keys[i];
-				var v = parts[keys[i]];
-				if (i === 0) {
-					this.url_var += encodeURI(k) + "=" + encodeURI(v);
-				} else {
-					this.url_var += "&" + encodeURI(k) + "=" + encodeURI(v);
-				}
-			}
-			this.url += this.url_var;
-		}
-
-		var req = new XMLHttpRequest();
-
-		//Bind event listeners
-		if(typeof this.progress !== "undefined"){
-			req.addEventListener("progress", this.progress, false);
-		}
-
-		if(typeof this.error !== "undefined"){
-			req.addEventListener("error", this.error, false);
-		}
-		
-		req.open(this.req_verb, this.url);
+		this.req.open(this.req_verb, this.url);
+		/*this has to be here, because it can only be called after open() is called*/
 		if(typeof this.headers !== "undefined"){
 			var keys = Object.keys(this.headers);
 			for (var i = 0; i < keys.length; i++) {
-				req.setRequestHeader(keys[i], this.headers[keys[i]]);
+				this.req.setRequestHeader(keys[i], this.headers[keys[i]]);
 			}
 		}
 		var parse_json = this.parse_json;
 		if (promise){
 			return new Promise(function(resolve, reject){
-				req.onload = function() {
+				this.req.onload = function() {
 					//on error - reject
 					if (parse_json === true){
 						return resolve(JSON.parse(this.response), this.status, this.getAllResponseHeaders());
@@ -63,20 +37,20 @@
 						return resolve(this.response, this.status, this.getAllResponseHeaders());
 					}
 				};
-				req.onerror = function() {
+				this.req.onerror = function() {
 					return reject(this.error_callback);
 				};
-				req.send();
+				this.req.send();
 			});
 		} else {
-			req.onload = function(){
+			this.req.onload = function(){
 				if (parse_json === true){
 					callback(JSON.parse(this.response), this.status, this.getAllResponseHeaders());
 				} else {
 					callback(this.response, this.status, this.getAllResponseHeaders());
 				}
 			};
-			req.send();
+			this.req.send();
 		}
 	};
 
@@ -86,7 +60,18 @@
 	};
 
 	Ajax.prototype.vars = function(vars){
-		this.url_var = vars;
+		var parts = "?";
+		var keys = Object.keys(vars);
+		for (var i = 0; i < keys.length; i++) {
+			var k = keys[i];
+			var v = vars[keys[i]];
+			if (i === 0) {
+				this.url_var += encodeURI(k) + "=" + encodeURI(v);
+			} else {
+				this.url_var += "&" + encodeURI(k) + "=" + encodeURI(v);
+			}
+		}
+		this.url += parts;
 		return this;
 	};
 
@@ -101,12 +86,12 @@
 	};
 
 	Ajax.prototype.progress = function(callback){
-		this.progress_callback = callback;
+		this.req.addEventListener("progress", callback, false);
 		return this;
 	};
 
 	Ajax.prototype.error = function(callback){
-		this.error_callback = callback;
+		this.req.addEventListener("error", callback, false);
 		return this;
 	};
 
